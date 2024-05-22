8898a83af65767ce5a8f849c28d9dc29d669f554
abhijeet.k <abhijeet.k@honeysys.com>
2024-05-22 23:26:38 +0530

    fix : github actions file

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,8 +7,8 @@ export const authOptions = {
   adapter: PrismaAdapter(db),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+      clientId: process.env.AUTH_GITHUB_ID ?? "",
+      clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
     }),
   ],
   session: {
